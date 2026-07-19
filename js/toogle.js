28e894f4161876807d5598528ea481a389edd67a
lucas dias xavier <lucasdx1993@outlook.com>
2026-07-19 00:29:43 -0300

    feat: add fontawesome cdn, implement active nav state, and refactor code

--- js/toogle.js
+++ js/toogle.js
@@ -6,3 +6,23 @@
       toggle.setAttribute('aria-expanded', isOpen);
       toggle.setAttribute('aria-label', isOpen ? 'Fechar menu' : 'Abrir menu');
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navItems = document.querySelectorAll('.nav-item');
+
+  navItems.forEach((item) => {
+    item.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      // Remove a classe e o atributo de todos os itens
+      navItems.forEach((nav) => {
+        nav.classList.remove('active');
+        nav.removeAttribute('aria-current');
+      });
+
+      // Adiciona a classe e o atributo apenas no item clicado
+      this.classList.add('active');
+      this.setAttribute('aria-current', 'page');
+    });
+  });
+});
